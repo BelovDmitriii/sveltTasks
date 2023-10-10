82a9c571db8c14214f2b25f6961dd86f8c22f44e
Belov Dmitrii <belovd90@mail.ru>
2023-10-11 01:34:21 +0300

    completed task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,23 @@
-import {encoded, translations} from './data.js'
+import {encoded, translations} from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decoded(array, translations) {
+  const decodedArray = [];
+  const idSet = new Set(); 
+  for (let i = 0; i < array.length; i++) {
+    const decodedObject = {};
+    for (let key in array[i]) {
+      if (key.includes("Id") && array[i][key] !== null && key !== 'groupId') {
+        decodedObject[key] = translations[array[i][key]];
+        idSet.add(array[i][key]);
+      } else {
+        decodedObject[key] = array[i][key];
+      }
+    }
+    decodedArray.push(decodedObject);
+  }
 
+  console.log("Уникальные id:", [...idSet].sort((a,b) => a - b));
+  return decodedArray;
+}
 
-
-// console.log(decoded)
+console.log(decoded(encoded, translations));
