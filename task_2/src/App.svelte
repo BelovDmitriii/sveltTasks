c5900996a022bda1b26b0fffbf4e4c29335923cc
Belov Dmitrii <belovd90@mail.ru>
2023-10-15 00:30:43 +0300

    added card and styles

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,122 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="container">
+    <div class="card">
+      <form>
+        <h1 class="container__title">Конвертер валют</h1>
+
+        <div class="courses">
+          <div class="course__item">
+            <div class="course__item__title">Курс USD</div>
+            <div class="course__item__value" data-value="USD">--.--</div>
+          </div>
+          <div class="course__item">
+            <div class="course__item__title">Курс EUR</div>
+            <div class="course__item__value" data-value="EUR">--.--</div>
+          </div>
+          <div class="course__item">
+            <div class="course__item__title">Курс RUB</div>
+            <div class="course__item__value" data-value="RUB">--.--</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="column">
+            <label for="name">Отдам:</label>
+            <select
+              class="form__select"
+              id="input"
+            >
+              <option value="USD">USD — Доллар США</option>
+              <option value="EUR">EUR — Евро</option>
+              <option value="RUB">RUB — Рубли</option>
+            </select>
+          </div>
+          <div class="column">
+            <label for="name">Получу:</label>
+            <select
+              id="result"
+              class="form__select"
+            >
+              <option value="USD">USD — Доллар США</option>
+              <option value="EUR">EUR — Евро</option>
+              <option value="RUB">RUB — Рубли</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="column">
+            <input
+              id="input"
+              type="number"
+              class="form__control"
+              autofocus
+            />
+          </div>
+          <div class="column">
+            <input
+              id="result"
+              type="number"
+              class="form__control"
+            />
+          </div>
+        </div>
+      </form>
+		</div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .container {
+    width: 40em;
+    border-radius: 10px;
+    background-color: lightgrey;
+    box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .container__title {
+    color: darkcyan;
+    margin: 0;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  .courses {
+    display: flex;
+    justify-content: space-between;
   }
-  .read-the-docs {
-    color: #888;
+
+  .course__item {
+    padding: 25px;
+  }
+
+  .course__item__title {
+    font-size: 24px;
+    color: indigo;
+  }
+
+  .course__item__value {
+    font-size: 20px;
+    color: red;
+  }
+
+  .row{
+    display: flex;
+    justify-content: space-around;
+    margin: 5px;
+    font-size: 20px;
+    color: #605858;
+  }
+
+  .form__select {
+    font-size: 17px;
+    background-color: #eae1e1;
+  }
+  .form__control {
+    font-size: 20px;
+    background-color: #eae1e1;
+    border-radius: 10px;
+    margin-top: 20px;
   }
 </style>
