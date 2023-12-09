0591f423399d37575a2a4e88eede8988f410ffd2
Belov Dmitrii <belovd90@mail.ru>
2023-12-09 22:09:52 +0300

    test

--- task_2/vite.config.ts
+++ task_2/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/BelovDmitriii/sveltTasks/tree/main/task_2/',
   plugins: [svelte()],
+  base: 'https://github.com/BelovDmitriii/sveltTasks/tree/main/task_2'
 })
